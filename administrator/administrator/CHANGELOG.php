e6be46ad42ce80c035301820ea5c01b240f62395
Ralf Meyer <ralf.meyer@mail.de>
2016-03-13 18:54:30 +0100

    bugfix

--- administrator/administrator/CHANGELOG.php
+++
@@ -1,7 +0,0 @@
-<?php defined('_JEXEC') or die(); ?>
-<h1>Firefighters Team Manager für das Joomla-CMS</h1>
-<h3>Changelog <small>Version 1.00</small></h3>
-<ul>
-<li> Start der Komponente </li>
-
-</ul>
