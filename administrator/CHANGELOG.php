90689ddd11ad0bbd227fdca90b14fe55fd9e52ea
Ralf Meyer <ralf.meyer@mail.de>
2016-10-10 09:33:32 +0200

    beta

--- administrator/CHANGELOG.php
+++ administrator/CHANGELOG.php
@@ -2,6 +2,12 @@
 <h1>Firefighters Team Manager für das Joomla-CMS</h1>
 <hr>
 
+<<<<<<< HEAD
+<h3>Changelog <small>Version 1.04_beta</small></h3>
+<ul>
++ Filtermöglichkeit im Menüpunkt Mitglieder 
+</ul>
+
 <h3>Changelog <small>Version 1.03</small></h3>
 <ul>
 + Main-Layout_2 für Mitglieder mit Modal-Detailansicht
@@ -16,6 +22,22 @@
 # Changlog-Anzeige
 </ul>
 
+=======
+<h3>Changelog <small>Version 1.03</small></h3>
+<ul>
++ Main-Layout_2 für Mitglieder mit Modal-Detailansicht
++ Detail-Layout_1 für Mitglied
++ Detail-Layout_1 für Termin
++ Auswahl main_layout und detail_layout bei Menüpunkt Termine
++ Auswahl main_layout und detail_layout bei Menüpunkt Mitglieder
+</ul>
+
+<h3>Changelog <small>Version 1.02</small></h3>
+<ul>
+# Changlog-Anzeige
+</ul>
+
+>>>>>>> refs/remotes/origin/master
 <h3>Changelog <small>Version 1.00</small></h3>
 <ul>
 + Start der Komponente
